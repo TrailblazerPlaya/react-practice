5c286ea8ae8e3bacde2d3162daaa54e1524a2667
fornara <fornarapro@gmail.com>
2023-09-13 14:35:04 +0300

    lesson 2 Counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
+
 import './App.css';
-import RandomNum from './components/RandomNum';
+
+import Button from './components/Button';
+import Counter from './components/Counter';
 function App() {
+  const [count, setCount] = useState(0);
+  const incrementCount = () => {
+    setCount(count +1);
+  }
   return (
     <div className="App">
-      <RandomNum />
+      <Counter count={count}/>
+      <Button  onClick={incrementCount}/> 
+      <Button  onClick={incrementCount}/> 
+      <Button  onClick={incrementCount}/> 
+      <Button  onClick={incrementCount}/> 
     </div>
   );
 }
--- src/utils/GenerateRandom.js
+++
@@ -1,3 +0,0 @@
-export default function generateRandomNum (min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
