c50e15c247855bb7ee0bf9e3a922b2a080f2fc16
fornara <fornarapro@gmail.com>
2023-09-15 18:42:32 +0300

    lesson 8 fetch Post

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
-import Login from "./components/Login";
+import Posts from "./components/Posts";
+
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Posts />
     </div>
   );
 }
